58e751ba2030f8f351c7ffdc2394290e140b7eab
[mark-ianz] <bustillomarkian23@gmail.com>
2024-09-09 21:58:29 +0800

    added a switch with a switch theme functionality

--- client/src/components/Navbar/Nav.tsx
+++ client/src/components/Navbar/Nav.tsx
@@ -1,19 +1,16 @@
-import { useTheme } from "../theme-provider";
+import DarkModeSwitch from "../DarkModeSwitch";
 import { Button } from "../ui/button";
 import { Input } from "../ui/input";
 
 export default function Nav() {
-  const { setTheme } = useTheme();
-
   return (
     <>
       <nav className="px-40 border-b">
         <div className="w-full flex">
           <h1>Logo</h1>
           <Input type="search" name="search" id="search" />
-          <Button className="px-8" onClick={()=> setTheme ("light")}>
-            Login
-          </Button>
+          <Button className="rounded-full px-8">Login</Button>
+          <DarkModeSwitch />
         </div>
       </nav>
     </>
